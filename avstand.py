bf0ca8d23c2e09e86bd1028596c50970c59f4ee3
androjon <jonfindahl@gmail.com>
2025-04-26 16:36:09 +0200

    Update avstand.py

--- avstand.py
+++ avstand.py
@@ -4,7 +4,7 @@ import requests
 import itertools
 import re
 import operator
-from import_ads_platsbanken import import_ads
+#from import_ads_platsbanken import import_ads
 
 @st.cache_data
 def import_data(filename):
@@ -160,8 +160,8 @@ def fetch_data():
     st.session_state.geodata = import_data("ort_ort_relevans.json")
     st.session_state.municipality_id_namn = import_data("kommun_id_namn.json")
     st.session_state.ad_data_historical = import_data("ssyk_region_kommun_annonser_2024.json")
-    st.session_state.ad_data_platsbanken = import_ads()
-    #st.session_state.ad_data_platsbanken= import_data("platsbanken.json")
+    #st.session_state.ad_data_platsbanken = import_ads()
+    st.session_state.ad_data_platsbanken= import_data("platsbanken.json")
     #st.session_state.labour_flow = import_data("labour_flow_data.json")
     #st.session_state.municipality_id_region_id = import_data("kommun_id_region_id.json")
     #st.session_state.forecast = import_data("barometer_regional.json")
